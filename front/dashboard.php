7c5b8077d1abb2916654788f678a2f1886495d87
Joel Fernando Dominguez Tapia <joeldomingueztapia@gmail.com>
2020-12-30 07:52:08 -0800

    Se añadio soporte a sass, se creo package.json dando soporte a npm solo en la carpeta front, sse creo dashboard.php y el esquema para el footer, se creo la carpeta src para contenido js y scss

---
+++ front/dashboard.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    
+    <link rel='stylesheet' href='./css/main.css' />
+  </head>
+  
+  <body class='dashboard'>
+    
+    <div class='dashboard__footer'>
+      <div class='dashboard__footerItem dashboard__footer--info'>
+        
+      </div>
+      
+      <div class='dashboard__footerItem dashboard__footer--employees'>
+        
+      </div>
+      
+      <div class='dashboard__footerItem dashboard__footer--contact'>
+        
+      </div>
+    </div>
+    
+  </body>
+  
+</html>
