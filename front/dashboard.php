22aa4e6c70e0ee59e5879c958398bff3dd8960c6
Ever Ponce <ever864@gmail.com>
2020-12-31 12:28:25 -0500

    Se agrego en dashboard.php las secciones header y body. Tambien, las configuraciones de esas partes en scss se creo el _header.scss y _body_scss

--- front/dashboard.php
+++ front/dashboard.php
@@ -9,7 +9,49 @@
   </head>
   
   <body class='dashboard'>
-    
+    <div class='dashboard__header'>
+      <div class='dashboard__header--burger'>
+            <h1>hello</h1>
+      </div>
+      <div class='dashboard__header--logo'>
+
+      </div>
+  </div>
+  <div class='dashboard__body'>
+    <div class='dashboard__bodyItem dashboard__body--front-daily'>
+
+    </div>
+    <div class='dashboard__bodyItem dashboard__body--notice1'>
+
+    </div>
+    <div class='dashboard__bodyItem dashboard__body--notice2'>
+
+    </div>
+    <div class='dashboard__bodyItem dashboard__body--notice3'>
+
+    </div>
+    <div class='dashboard__bodyItem dashboard__body--notice4'>
+
+    </div>
+    <div class='dashboard__bodyItem dashboard__body--publicity'>
+
+    </div>
+    <div class='dashboard__bodyItem dashboard__body--front-weekly'>
+
+    </div>
+    <div class='dashboard__bodyItem dashboard__body--notice5'>
+
+    </div>
+    <div class='dashboard__bodyItem dashboard__body--notice6'>
+
+    </div>
+    <div class='dashboard__bodyItem dashboard__body--notice7'>
+
+    </div> 
+    <div class='dashboard__bodyItem dashboard__body--notice8'>
+
+    </div>      
+  </div>
     <div class='dashboard__footer'>
       <div class='dashboard__footerItem dashboard__footer--info'>
         
